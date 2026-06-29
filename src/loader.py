b31b50d7cbbd2b5426ecb91646f0eb7541d586f2
firas_farhani <firass.farhani@enicar.ucar.tn>
2026-06-29 18:49:15 +0100

    feature : add load_excel function

--- src/loader.py
+++ src/loader.py
@@ -23,17 +23,32 @@ def load_excel(file_path):
     excel_extensions = (".xlsx",".xlsm",".xls",".xlsb")    
     if (not os.path.isfile(file_path)) :
         raise FileNotFoundError(f"File not found : {file_path}")
+    #check the type of the file
     if file_path.lower().endswith(excel_extensions):
         df = pd.read_excel(file_path)
     elif file_path.lower().endswith(".csv"):
         df = pd.read_csv(file_path)
+    else:
+        raise ValueError("Unsupported file type. Please provide and Excel or CSV file.")
     
+    #Convert rows into langchain documents
+
     documents = []
-    for index,row  in df.iterrows:
-        text = row.dopna().to_string()
+    for index,row  in df.iterrows() :
+        #data cleaning
+        text = ", ".join([f"{col}: {val}" for col, val in row.dropna().items()])
 
         if text.strip() :
-            doc = Document
+            doc = Document(
+                page_content=text,
+                metadata = {
+                    "source" : file_path,
+                    "row" : index
+                }
+            )
+            documents.append(doc)
+
+    return documents        
 
 
 
