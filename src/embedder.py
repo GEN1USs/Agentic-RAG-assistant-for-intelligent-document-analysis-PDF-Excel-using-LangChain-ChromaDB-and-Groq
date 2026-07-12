222fef853033495be7e215b6ff40ead787820610
firas_farhani <firass.farhani@enicar.ucar.tn>
2026-07-12 16:28:11 +0100

    feat: add emebedding model lodaer using HuggingFace

---
+++ src/embedder.py
@@ -0,0 +1,10 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+def get_embedding_model():
+    model = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs = {"device" : "cpu"},
+        encode_kwargs = {"normalize_embeddings" : True}
+    )
+    return model
+    
