1e3541784afdfcd4390562e4bf69bf7883a61472
firas_farhani <firass.farhani@enicar.ucar.tn>
2026-08-18 22:31:14 +0100

    fix the icon and make the code run

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
-import streamlit as st
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import streamlit as st
+import tempfile
+from src.agent import ask
+from src.retriever import build_vector_store
+from src.embedder import get_embedding_model
+from src.chunker import split_documents
+from src.loader import load_excel, load_pdf, load_txt
 
 st.set_page_config(
     page_title="Document Assistant",
-    page_icon="...",
+    page_icon="https://res.cloudinary.com/dtz0urit6/image/upload/q_auto:best,f_jpg/cloudinary-tools-uploads/ccuvnpuisgs2t1ulzkxp",
     layout="wide"
 )
-
