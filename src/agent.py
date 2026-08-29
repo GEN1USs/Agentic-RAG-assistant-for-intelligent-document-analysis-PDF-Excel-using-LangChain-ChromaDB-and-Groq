656b23b9b55f6f5088cb34ea85351f847478cad8
firas_farhani <firass.farhani@enicar.ucar.tn>
2026-08-29 13:03:29 +0100

    feat: add the ability for agent to handle small talks

--- src/agent.py
+++ src/agent.py
@@ -30,13 +30,34 @@ def detect_prompt(question , file_type):
         return QA_PROMPT
 
 def ask(question,vector_store,file_type="pdf"):
+    #initiate the LLM model
+    llm = ChatGroq(
+        model="openai/gpt-oss-120b",
+        groq_api_key=get_api_key(),
+        temperature=0
+        )
+    #classify the question 
+
+    classification_prompt = f"""You are a classifier. Classify the following message into one of two categories:
+- "small talk" : if the message is a greeting, farewell, thanks,casual conversation ,or anything NOT related to analyzing a document
+- "document" : if the message is asking about the content of a document, requesting analysis ,summary, or info extraction
+Message: {question}
+Reply with ONLY one word : either "small_talk" or "document". Nothing else."""
+    classification = llm.invoke(classification_prompt).content.strip().lower()
+    #check if it is small talk
+    if "small_talk" in classification:
+        small_talk_prompt = f"""You are a friendly document analysis assisstant. The user said "{question}" 
+        Respond anturally and briefly.Mention that you are a document assistant and can help analyze PDFs,Excel files , and text documents.
+        Keep it short and friendly."""
+        response = llm.invoke(small_talk_prompt).content
+        return {
+            "answer" : response,
+            "sources" : []
+        }
+    #step 3 : if document question ,run the full RAG pipline
     prompt = detect_prompt(question,file_type)
     chunks = vector_store.similarity_search(question,k=3)
-    llm = ChatGroq(
-    model="openai/gpt-oss-120b",
-    groq_api_key=get_api_key(),
-    temperature=0
-    )
+    
     chain = prompt | llm | StrOutputParser()
     context,sources = format_context(chunks)
     answer = chain.invoke(
